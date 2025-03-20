9fda80a052fc2255352e35b70296906a52d995ad
SarahQuoos <sarah.repeat@gmail.com>
2025-03-20 17:44:46 -0300

    Update fluxodecaixa.py

--- fluxodecaixa.py
+++ fluxodecaixa.py
@@ -13,6 +13,11 @@ from datetime import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+#Identificando arquivo do drive e as credenciais
+scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name('credencial_caminhosdamoda.json',scope)
+client = gspread.authorize(creds)
+
 #Configuração de Página
 st.set_page_config(
     page_title="Caminhos da Moda",
@@ -26,3 +31,21 @@ st.title("Fluxo de Caixa Caminhos da Moda")
 
 #Subtítulo
 st.markdown("### Consulta produtos disponíveis")
+
+#Abrindo arquivo no drive e transformando em dataframe no pandas
+arquivo = client.open('Fluxodecaixa_Caminhosdamoda')
+sheet = arquivo.worksheet("Lista Produtos")
+
+data = sheet.get_all_values()
+colunas = data.pop(0)
+listaprodutos = pd.DataFrame(data,columns=colunas)
+
+arquivo.client.session.close()
+
+#Fazendo filtro
+query = st.text_input("Filtro")
+if query:
+    mask = listaprodutos.applymap(lambda x: query in str(x).upper()).any(axis=1)
+    listaprodutos = listaprodutos[mask]
+
+st.data_editor(listaprodutos,hide_index=True,) 
