1cd65adb7d2623031386dc3fc6130e8c28c9b9cf
SarahQuoos <sarah.repeat@gmail.com>
2025-06-26 14:17:38 -0300

    Update fluxodecaixa.py

--- fluxodecaixa.py
+++ fluxodecaixa.py
@@ -323,8 +323,8 @@ with st.expander("Conferir Fluxo de Caixa"):
         despesas_mensais = listadespesas.groupby('Mês/Ano')['Valor Despesa'].sum().reset_index()
 
         #Juntando os dados filtrados por mês
-        dados_mensal = pd.merge(pecas_mensais, pecas_mensais_aux, on='Mês/Ano', how='outer')
-        dados_mensal = pd.merge(dados_mensal, ganhos_mensais, on='Mês/Ano', how='outer')
+        pecas_mensais.append_row(pecas_mensais_aux)
+        dados_mensal = pd.merge(pecas_mensais, ganhos_mensais, on='Mês/Ano', how='outer')
         dados_mensal = pd.merge(dados_mensal, despesas_mensais, on='Mês/Ano', how='outer')
 
         #Renomeando dados e colocando 0 no lugar de células vazias
