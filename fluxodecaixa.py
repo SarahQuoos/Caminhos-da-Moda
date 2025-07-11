2dc669b25f062fdcef0ffe7634de38e8b23b0bc4
SarahQuoos <sarah.repeat@gmail.com>
2025-07-11 11:25:14 -0300

    Update fluxodecaixa.py

--- fluxodecaixa.py
+++ fluxodecaixa.py
@@ -62,14 +62,13 @@ def Cadastro():
         numeracao = st.selectbox("Numeração:",("Select","ÚNICO","PP","P","M","G","GG","34","35","36","37","38","40","42","44","46"),)
         with st.expander("Revenda de peça?"): 
             valorpago = st.number_input('Valor Pago na peça:', value=0.00)
+            valor_sugestao_rev = ((valorpago+5)*1.19)*1.5
+            #0,1349(taxas crédito 10x) + 0,05(gastos gerais embutidos) = 0,19
+            st.metric(label="Valor Mínimo de Revenda", value=f"{'R$ {:,.2f}'.format(valor_sugestao_rev)} ",)
         with st.expander("Peça Consignada?"):
             valoretorno = st.number_input('Porcentagem Consignação:')
         with st.expander("Valor Sugerido de Venda:"):
-            if valorpago != 0:
-                valor_sugestao_rev = ((valorpago+5)*1.19)*1.5
-                #0,1349(taxas crédito 10x) + 0,05(gastos gerais embutidos) = 0,19
-                st.metric(label="Valor Mínimo de Revenda", value=f"{'R$ {:,.2f}'.format(valor_sugestao_rev)} ",)
-            else:
+            if valorpago == 0:
                 valor_pensado = st.number_input('Valor Ideia de Ganho:', value=0.00)
                 valor_pensado_venda = st.number_input('Valor Ideia de Venda:', value=0.00)
                 if valor_pensado != 0:
@@ -86,7 +85,7 @@ def Cadastro():
         
         #Botões de cadastro
         if st.form_submit_button("Cadastrar"):
-            if (categoria == "Select") or (proprietario == "") or (produto == "") or (valor == 0) or (valor_pensado == 0):
+            if (categoria == "Select") or (proprietario == "") or (produto == "") or (valor == 0):
                 st.write("Preencha todas as informações para cadastro")
             else:
                 sheet1.append_row(cadastro)
